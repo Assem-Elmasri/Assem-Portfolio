1ee243d78c704f884ebe11d8dba17a3a813dedee
Assem-Elmasri <aessemelmasry55@gmail.com>
2025-09-04 17:04:35 +0300

    completing the portfolion -- still need an upgrade

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // bar menu toggle
 const bar = document.querySelector(".fa-bars");
 const ul = document.querySelector("nav ul");
@@ -9,9 +11,32 @@ bar.addEventListener("click", () => {
 const hireMeBtn = document.getElementById("hire-me-btn");
 const popUp = document.querySelector("div.fixed");
 const closePopUp = document.querySelector("div.fixed form button#close-btn");
-hireMeBtn.addEventListener("click", () => {
+hireMeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   popUp.classList.toggle("hidden");
 });
 closePopUp.addEventListener("click", () => {
   popUp.classList.toggle("hidden");
+});
+
+
+const contact = document.getElementById("contact");
+contact.addEventListener("click", (e) => {
+  e.preventDefault();
+  popUp.classList.toggle("hidden");
+});
+
+// send email
+
+document.querySelector("#contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_lodk7no', 'template_k8y98aq', this)
+    .then(() => {
+      alert("Message sent successfully!");
+      document.querySelector("#contact-form").reset();
+      document.querySelector("#contact-form").parentElement.classList.toggle("hidden");
+    }, (error) => {
+      alert("Failed to send message: " + JSON.stringify(error));
+    });
 });
